c8b8fdffce52c01d3d6f813c10ab5e2a08207bef
SeungKyu0872 <109332052+SeungKyu0872@users.noreply.github.com>
2022-07-19 17:46:36 +0900

    Add files via upload

---
+++ 0719/0719_hw.py
@@ -0,0 +1,13 @@
+# 1번
+"""
+mutable = set , dictonary , list
+immutkble = string , tuple , range
+"""
+
+# 2번
+
+student = {'유승규' : 27 , '최현웅' : 26 , '나웅기' : 28}
+
+# 3번
+scores = [80 , 89 , 99 ,83]
+print(sum(scores)/len(scores))
---
+++ 0719/0719_ws.py
@@ -0,0 +1,10 @@
+# 1번
+
+a = int(input()) 
+b = int(input())
+print(a+b)
+
+# 2번
+
+lunch = {'계란' : 500 , '피자' : 20000 , '치킨' : 20000 , '닭가슴살' : 2000 }
+print(sum(lunch.values())/(len(lunch)))
